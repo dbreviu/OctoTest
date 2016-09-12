99e7ae6e474ca99bff38acbe0faf836083d7cb69
djbrevi <djbrevi@MD2LAPJWBD562>
2016-09-12 11:13:04 -0500

    test2

--- src/OctoTest/Startup.cs
+++ src/OctoTest/Startup.cs
@@ -30,7 +30,7 @@ namespace OctoTest
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World! This is doge");
+                await context.Response.WriteAsync("Hello World! This is doge version 2!");
             });
         }
     }
